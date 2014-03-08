f6dc08fb79a71b4a26f7cc71d8a4d6905884c5c1
tomallen400 <tomallen4000@gmail.com>
2014-03-08 16:51:11 +0000

    add begin and end to areas

--- lib/wrapper/resources/area.rb
+++ lib/wrapper/resources/area.rb
@@ -1,6 +1,6 @@
 class Musicbrainz::Area
 	
-	attr_accessor :id, :name, :sort_name, :disambiguation, :iso_3166_3_codes, :iso_3166_2_codes, :iso_3166_1_codes
+	attr_accessor :id, :name, :sort_name, :disambiguation, :iso_3166_3_codes, :iso_3166_2_codes, :iso_3166_1_codes, :begin, :end, :ended
 	
 	def initialize args
 		args.each do |k, v|
